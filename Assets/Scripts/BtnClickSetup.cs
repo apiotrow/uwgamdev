38b8a345f5abcec9b57ef09ff821cbe95210cac3
Aaron Piotrowski <aaron.p.piotrowski@gmail.com>
2015-11-30 21:32:48 -0800

    button selection manager half done

---
+++ Assets/Scripts/BtnClickSetup.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class BtnClickSetup : MonoBehaviour {
+	public List<Button> scriptBtns;
+	public List<Button> bankBtns;
+
+	void Start () {
+		scriptBtns = new List<Button>();
+		bankBtns = new List<Button>();
+		GameObject btnsGO;
+
+		//set up script buttons
+		btnsGO = GameObject.Find("ScriptBtns");
+		for(int i = 0; i < btnsGO.transform.childCount; i++){
+			Transform ch = btnsGO.transform.GetChild(i);
+			Button btn = ch.transform.Find("BG").gameObject.GetComponent<Button>();
+			btn.onClick.AddListener(() => {
+				SelectionManager.selectScriptBtn(btn);
+			});
+			scriptBtns.Add(btn);
+
+			SelectionManager.initColorBlock = btn.colors;
+		}
+
+		//set up bank buttons
+		btnsGO = GameObject.Find("BankBtns");
+		for(int i = 0; i < btnsGO.transform.childCount; i++){
+			Transform ch = btnsGO.transform.GetChild(i);
+			Button btn = ch.transform.Find("BG").gameObject.GetComponent<Button>();
+			btn.onClick.AddListener(() => {
+				SelectionManager.selectBankBtn(btn);
+			});
+			bankBtns.Add(btn);
+		}
+
+
+	}
+	void Update(){
+//		print(SelectionManager.bankBtnNow.gameObject.GetInstanceID());
+	}
+	
+}
---
+++ Assets/Scripts/SelectionManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public static class SelectionManager {
+	public static Button scriptBtnNow;
+	public static Button bankBtnNow;
+	public static ColorBlock initColorBlock;
+//	public static List<Button> scriptBtns;
+//	public static List<Button> bankBtns;
+
+	public static void selectScriptBtn(Button btn){
+		scriptBtnNow = btn;
+
+		ColorBlock cb = btn.colors;
+		cb.normalColor = Color.red;
+		cb.highlightedColor = Color.red;
+		btn.colors = cb;
+
+		//unselect every other button
+		GameObject btnsGO = GameObject.Find("ScriptBtns");
+		for(int i = 0; i < btnsGO.transform.childCount; i++){
+			Transform ch = btnsGO.transform.GetChild(i);
+			Button currBtn = ch.transform.Find("BG").gameObject.GetComponent<Button>();
+			if(currBtn.gameObject.GetInstanceID() != scriptBtnNow.gameObject.GetInstanceID()){
+				currBtn.colors = initColorBlock;
+			}
+		}
+	}
+
+	public static void selectBankBtn(Button btn){
+		bankBtnNow = btn;
+
+		ColorBlock cb = btn.colors;
+		cb.normalColor = Color.red;
+		cb.highlightedColor = Color.red;
+		btn.colors = cb;
+
+		//unselect every other button
+		GameObject btnsGO = GameObject.Find("BankBtns");
+		for(int i = 0; i < btnsGO.transform.childCount; i++){
+			Transform ch = btnsGO.transform.GetChild(i);
+			Button currBtn = ch.transform.Find("BG").gameObject.GetComponent<Button>();
+			if(currBtn.gameObject.GetInstanceID() != bankBtnNow.gameObject.GetInstanceID()){
+				currBtn.colors = initColorBlock;
+			}
+		}
+	}
+
+}
